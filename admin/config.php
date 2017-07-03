33afc231110d3cec65a08bacea89c7fc9ac27932
nrjgit <mail.nrj@gmail.com>
2017-07-03 18:11:32 +0530

    site added

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://uaeshoper.com/admin/');
+define('HTTP_CATALOG', 'https://uaeshoper.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://uaeshoper.com/admin/');
+define('HTTPS_CATALOG', 'https://uaeshoper.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/uaeshoper/public_html/admin/');
+define('DIR_SYSTEM', '/home/uaeshoper/public_html/system/');
+define('DIR_IMAGE', '/home/uaeshoper/public_html/image/');
+define('DIR_CATALOG', '/home/uaeshoper/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'uaeshoper_oca927');
+define('DB_PASSWORD', '1.5@K3Mp6S');
+define('DB_DATABASE', 'uaeshoper_oca927');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc5p_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://uaeshoper.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://uaeshoper.com/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/uaeshoper/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/uaeshoper/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/uaeshoper/image/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'uaeshoper_oca927');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc5p_');
